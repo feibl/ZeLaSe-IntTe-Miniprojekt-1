1768e05faefb7268348a0a130127df92fa10d9a2
Fabian Senn <fsenn@hsr.ch>
2012-10-22 10:09:17 +0200

    Chat und User Managed Beans created

---
+++ IntTe_MP_01/src/chatlobby/Chat.java
@@ -0,0 +1,11 @@
+package chatlobby;
+
+public class Chat {
+	private String theme;
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+}
