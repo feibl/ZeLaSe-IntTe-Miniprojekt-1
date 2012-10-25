57f4a78f9036f22cb2dcec8a67f2d0e0f24fa450
Fabian Senn <fsenn@hsr.ch>
2012-10-25 10:20:39 +0200

    Register Form

--- IntTe_MP_01/src/chatlobby/UserManager.java
+++ IntTe_MP_01/src/chatlobby/UserManager.java
@@ -6,9 +6,13 @@ import java.util.*;
 public class UserManager {
 	protected Map<String, User> users = new Hashtable<String, User>();
 
-	protected String userFilename = "/home/feibl/temp/users.ser";
+	protected String userFilename = "users.ser";
 
 	protected File userFile;
+	
+	private String username;
+	private String password;
+	private String pw_verification;
 
 	public UserManager() {
 		userFile = new File(userFilename);
@@ -24,7 +28,14 @@ public class UserManager {
 		}
 	}
 
-	public boolean register(User user) throws UserException {
+	public boolean register() throws UserException {
+		System.out.println("register");
+		if(!pw_verification.equals(password)) {
+			throw new UserException();
+		}
+		User user = new User();
+		user.setName(username);
+		user.setPassword(password);
 		if (users.get(user.getName()) != null) { // User existiert bereits
 			throw new UserException();
 		}
@@ -59,5 +70,30 @@ public class UserManager {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public String getUsername() {
+		System.out.println("hallo");
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPw_verification() {
+		return pw_verification;
+	}
+
+	public void setPw_verification(String pw_verification) {
+		this.pw_verification = pw_verification;
 	}
 }
