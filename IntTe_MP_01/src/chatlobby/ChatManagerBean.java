22da0dadd43405b372347b8fa41c273927aba42a
Fabian Senn <fsenn@hsr.ch>
2012-10-29 16:38:29 +0100

    Chatname Validation

--- IntTe_MP_01/src/chatlobby/ChatManagerBean.java
+++ IntTe_MP_01/src/chatlobby/ChatManagerBean.java
@@ -2,26 +2,28 @@ package chatlobby;
 
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 
 public class ChatManagerBean {
 	protected Map<String, Chat> chatrooms = new Hashtable<String, Chat>();
 
-
-	
-
 	public ChatManagerBean() {
-		
+
 	}
 
 	public int getNumberOfChats() {
 		return chatrooms.size();
 	}
 
-
 	public Map<String, Chat> getChatrooms() {
 		return chatrooms;
 	}
-	
+
 	public void setChatrooms(Map<String, Chat> chatrooms) {
 		this.chatrooms = chatrooms;
 	}
@@ -34,9 +36,15 @@ public class ChatManagerBean {
 		newChat.setTopic(name);
 		chatrooms.put(name, new Chat());
 
-		
 	}
 
-	
+	public void validateChatName(FacesContext context, UIComponent toValidate,
+			Object value) {
+		if (chatrooms.get(value.toString()) != null) {
+			((UIInput)toValidate).setValid(false);
+			context.addMessage(toValidate.getClientId(context),
+					new FacesMessage("Chat with topic "+value.toString()+" already created"));
+		}
+	}
 
 }
